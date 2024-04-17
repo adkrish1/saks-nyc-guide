b4f419a97c403efe9985e672623b08c9cd4314cd
swaran18999 <swaran18999@gmail.com>
2024-04-17 16:34:26 -0400

    chat from DB added

--- mobile-app/saks_nyc_guide/lib/pages/chatbot.dart
+++ mobile-app/saks_nyc_guide/lib/pages/chatbot.dart
@@ -1,11 +1,12 @@
-import 'dart:convert';
-
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart' show rootBundle;
 import 'package:flutter_chat_types/flutter_chat_types.dart' as types;
 import 'package:flutter_chat_ui/flutter_chat_ui.dart';
+import 'package:saks_nyc_guide/utils/messages_util.dart';
 import 'package:uuid/uuid.dart';
+import 'dart:async';
 
+import 'package:path/path.dart';
+import 'package:sqflite/sqflite.dart';
 class ChatBotPage extends StatefulWidget {
 	const ChatBotPage({super.key});
 
@@ -19,10 +20,24 @@ class _ChatPageState extends State<ChatBotPage> {
 		id: '82091008-a484-4a89-ae75-a22bf8d6f3ac',
 	);
 
+		dynamic database;
+
+		Future<void> createDB() async{
+			database = openDatabase(
+				join(await getDatabasesPath(), 'messages_database.db'),
+				onCreate: (db, version) {
+					return db.execute(
+						'CREATE TABLE messages(id STRING PRIMARY KEY, messageText TEXT, createdAt INTEGER, author STRING)',
+					);
+				},
+				version: 1,
+			);
+		}
+
 	@override
 	void initState() {
 		super.initState();
-		_loadMessages();
+			createDB().then((value) => _loadMessages());
 	}
 
 	void _addMessage(types.Message message) {
@@ -31,7 +46,7 @@ class _ChatPageState extends State<ChatBotPage> {
 		});
 	}
 
-	void _handleSendPressed(types.PartialText message) {
+	void _handleSendPressed(types.PartialText message) async{
 		final textMessage = types.TextMessage(
 			author: _user,
 			createdAt: DateTime.now().millisecondsSinceEpoch,
@@ -39,17 +54,36 @@ class _ChatPageState extends State<ChatBotPage> {
 			text: message.text,
 		);
 
+			await database.then((d) {
+				d.insert(
+					"messages", 
+					ChatMessage(author: textMessage.author, createdAt: textMessage.createdAt, id: textMessage.id, messageText: textMessage.text).toMap()
+				);
+			});
+
 		_addMessage(textMessage);
 	}
 
 	void _loadMessages() async {
-		final response = await rootBundle.loadString('assets/messages.json');
-		final messages = (jsonDecode(response) as List)
-			.map((e) => types.Message.fromJson(e as Map<String, dynamic>))
-			.toList();
-
-		setState(() {
-			_messages = messages;
+		await database.then((d) {
+			d.query('messages').then((messagesMap) {
+			for (final {
+				'id': id as String,
+				'author': author as String,
+				'messageText': messageText as String,
+				'createdAt': createdAt as int?,
+			} in messagesMap) {
+				final message = types.TextMessage(
+				author: types.User(id: author),
+				createdAt: createdAt as int,
+				id: id,
+				text: messageText,
+				);
+					setState(() {
+						_messages.insert(0, message);
+					});
+			}
+			});
 		});
 	}
 
@@ -68,5 +102,5 @@ class _ChatPageState extends State<ChatBotPage> {
 				),
 			),
 		),
-		);
+	);
 }
