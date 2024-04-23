adeb26f9688f7e2c6e688732c964d30ad51ba4f8
swaran18999 <swaran18999@gmail.com>
2024-04-23 13:36:38 -0400

    settings page created

--- mobile-app/saks_nyc_guide/lib/pages/settings.dart
+++ mobile-app/saks_nyc_guide/lib/pages/settings.dart
@@ -1,10 +1,38 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:sqflite/sqflite.dart';
+import 'package:path/path.dart';
 
 class SettingsPage extends StatelessWidget {
   const SettingsPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+        body: Center(
+            child: ListView(
+      children: const <Widget>[
+        ListTile(
+          title: Text('Info'),
+          dense: true,
+        ),
+        ListTile(
+          title: Text('Clear Chats'),
+          onTap: clearChats,
+          dense: true,
+        ),
+        ListTile(
+          title: Text('Logout'),
+          dense: true,
+        ),
+      ],
+    )));
   }
+}
+
+void clearChats() async {
+  final db = await openDatabase(
+    join(await getDatabasesPath(), 'messages_database.db'),
+  );
+  await db.execute("DELETE FROM messages");
 }
